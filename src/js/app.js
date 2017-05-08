dd5d71f918e69a4fcfa7400c5ca6eafec6e542a3
paweltar <paweltar@gmail.com>
2017-05-08 13:33:58 +0200

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import $ from 'jquery';
+import uikitCss from 'uikit/dist/css/uikit.css'
+
+UIkit.use(Icons);
+
+$(document).ready(function() {
+  console.log('hello');
+});

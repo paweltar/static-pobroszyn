2e5270065b8f61ad165ffebee65fbbac539cb0ba
paweltar <paweltar@gmail.com>
2017-05-15 14:00:26 +0200

    Prepare top section

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,25 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import $ from 'jquery';
-import uikitCss from 'uikit/dist/css/uikit.css'
+import uikitCss from 'uikit/dist/css/uikit.css';
+import Leaflet from 'leaflet/dist/leaflet.js';
+import LeafletCss from 'leaflet/dist/leaflet.css';
 
 UIkit.use(Icons);
 
 $(document).ready(function() {
-  console.log('hello');
+  var mymap = Leaflet.map('map').setView([50.786394, 21.462547], 10);
+  Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF3ZWx0YXIiLCJhIjoiY2oycTBxd2F6MDJwNTJybWg4b3g1Y2lqcSJ9.GI_6RfChSTz01THupQNBlQ', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.outdoors',
+    accessToken: 'pk.eyJ1IjoicGF3ZWx0YXIiLCJhIjoiY2oycTBxd2F6MDJwNTJybWg4b3g1Y2lqcSJ9.GI_6RfChSTz01THupQNBlQ'
+}).addTo(mymap);
+
+this.customDefault = Leaflet.icon({
+		iconUrl: 'images/leaflet/marker-icon.png',
+		shadowUrl: 'images/leaflet/marker-shadow.png',
+	});
+
+var marker = Leaflet.marker([50.786394, 21.462547], { icon: this.customDefault }).addTo(mymap);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
             }, {
                 test: /\.css/,
                 loaders: ["style-loader", "css-loader"]
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false']
             }
         ]
     }

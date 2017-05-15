c6a12bc4a89fea9d3c72dc5656d5d57a469faeba
paweltar <paweltar@gmail.com>
2017-05-15 14:31:01 +0200

    Prepare main section

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import $ from 'jquery';
+import slick from 'slick-carousel';
 import uikitCss from 'uikit/dist/css/uikit.css';
+import slickCss from 'slick-carousel/slick/slick.css';
 import Leaflet from 'leaflet/dist/leaflet.js';
 import LeafletCss from 'leaflet/dist/leaflet.css';
 
@@ -22,4 +24,17 @@ this.customDefault = Leaflet.icon({
 	});
 
 var marker = Leaflet.marker([50.786394, 21.462547], { icon: this.customDefault }).addTo(mymap);
+
+$('.gallery__slides').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3500,
+      infinite: true,
+      draggable: true,
+      prevArrow: '.slidenav--left',
+      nextArrow: '.slidenav--right',
+      speed: 600,
+      cssEase: 'cubic-bezier(0.445, 0.05, 0.55, 0.95)'
+    });
 });
